3553d1fa73140aa41370324b0bb7f316cb56a890
sharat910 <sharat910@gmail.com>
2016-10-26 23:09:15 +0530

    minor change

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from classifier import Classifier
 import csv
 import random
 
-categories = ['Energy and Environment', 'Material Science', 'Signal Processing', 'Economics and Finance', 'Health and Nutrition', 'Software Design']
-
-clf = Classifier(categories)
+categories = []
 
 data = []
+
 with open('data.csv') as csvfile:
 	reader = csv.DictReader(csvfile,fieldnames = ["abstract","category"])
 	for row in reader:
 		data.append({'abstract':row['abstract'],'category':row['category']})
+		if row['category'] not in categories:
+			categories.append(row['category'])
 
-print len(data)
 random.shuffle(data)
 
 train_data = data[:200]
 test_data = data[200:]
+
+clf = Classifier(categories)
 
 #Training
 for item in train_data:
@@ -28,7 +30,7 @@ arr = []
 for item in test_data:
 	arr.append(clf.test(item['abstract'],item['category']))
 
-
+#Accuracy
 print float(sum(arr))/len(arr)
 		 
 
